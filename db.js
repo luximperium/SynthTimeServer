c5167186f23ce01b4403522bba2db8525b161d59
Nathaniel Atkins <64879234+luximperium@users.noreply.github.com>
2020-09-08 14:05:07 -0400

    Heroku Time

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('synthtime', 'postgres', process.env.PASS, {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
 });
 
