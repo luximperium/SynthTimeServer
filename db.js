4fe3739e6ca18046e7d88e962420480c23b9c78a
Nathaniel Atkins <64879234+luximperium@users.noreply.github.com>
2020-09-09 10:58:15 -0400

    Database associations finished!

--- db.js
+++ db.js
@@ -1,21 +1,28 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
+  dialect: "postgres",
 });
 
 sequelize.authenticate().then(
-    function() {
-        console.log('Connected to SynthTime postgres database');
-    },
-    function(err){
-        console.log('Connection to database failed!')
-    }
+  function () {
+    console.log("Connected to SynthTime postgres database");
+  },
+  function (err) {
+    console.log("Connection to database failed!");
+  }
 );
 
-// let Users = sequelize.import('./models/users');
-// let Projects = sequelize.import('./models/project')
+let Users = sequelize.import("./models/users");
+let Projects = sequelize.import("./models/project");
 
-// Users.hasMany(Projects);
-// Projects.belongsTo(Users);
+Users.hasMany(Projects, {
+  foreignKey: "owner",
+  constraints: true,
+});
+
+Projects.belongsTo(Users, {
+  foreignKey: "id",
+  constraints: true,
+});
 
 module.exports = sequelize;
